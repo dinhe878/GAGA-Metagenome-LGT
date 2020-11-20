d9fdeb8cac8897659f5508355dec3167c5ad3eb5
dinhe878 <derrick.he@gmail.com>
2020-11-20 14:15:53 +0100

    dev

--- download_BLASTdb.sh
+++ download_BLASTdb.sh
@@ -9,10 +9,13 @@ wget ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt
 
 # generate esearch query for all chromosome level (highest assemly quality) genomes: 3307 on 20 Nov 2020
 # cat prokaryotes.txt|awk -F '\t' '{if ($16=="Chromosome") print $0}'|wc -l
+echo "generating esearch query..."
 query=$(cat prokaryotes.txt|awk -F '\t' '{if ($16=="Chromosome") print $0}'|cut -f 19|sed 1d|tr "\n" " "|perl -pe 's/ / OR /g'|perl -pe "s/ OR $//g")
 
 # retrieve genomes
+echo "dowdloading genomes..."
 esearch -db assembly -query "$query"|elink -target nuccore|efetch -format fasta > db.prok_3307genomes_20112020.fa
 
 # retrieve proteomes
+echo "dowdloading proteomes..."
 esearch -db assembly -query "$query"|elink -target nuccore|efetch -format fasta_cds_aa > db.prok_3307genomes_20112020.proteins.fa
