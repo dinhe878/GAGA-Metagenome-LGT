f4e49c470e8cb881d66982beca4a52a2523bd9c4
dinhe878 <derrick.he@gmail.com>
2020-11-20 14:11:14 +0100

    dev

--- download_BLASTdb.sh
+++ download_BLASTdb.sh
@@ -1,5 +1,5 @@
 # load the compoterome modules
-module load tools edirect/7.50
+module load tools perl edirect/7.50
 
 # go to the BLASTdb folder
 cd /home/people/dinghe/ku_00039/people/dinghe/BLASTdb
@@ -9,7 +9,7 @@ wget ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt
 
 # generate esearch query for all chromosome level (highest assemly quality) genomes: 3307 on 20 Nov 2020
 # cat prokaryotes.txt|awk -F '\t' '{if ($16=="Chromosome") print $0}'|wc -l
-query=$(cat prokaryotes.txt|awk -F '\t' '{if ($16=="Chromosome") print $0}'| |cut -f 19|sed 1d|tr "\n" " "|perl -pe 's/ / OR /g'|perl -pe "s/ OR $//g")
+query=$(cat prokaryotes.txt|awk -F '\t' '{if ($16=="Chromosome") print $0}'|cut -f 19|sed 1d|tr "\n" " "|perl -pe 's/ / OR /g'|perl -pe "s/ OR $//g")
 
 # retrieve genomes
 esearch -db assembly -query "$query"|elink -target nuccore|efetch -format fasta > db.prok_3307genomes_20112020.fa
