60178fd71471768ffeedff999bf255fc65811b07
dinhe878 <derrick.he@gmail.com>
2020-11-26 22:54:54 +0100

    minor fix

--- download_BLASTdb.sh
+++ download_BLASTdb.sh
@@ -37,42 +37,42 @@ cat > retrieve_PATRIC_prok_geneomes.qsub <<EOF
 ### Requesting time - format is <days>:<hours>:<minutes>:<seconds>
 #PBS -l walltime=10:00:00
 ### Set up the environmental variables
-#PBS -V $NCBI_API_KEY=593466ec18bd4cdf7c4cfd2ea54ff3908209
+#PBS -V \$NCBI_API_KEY=593466ec18bd4cdf7c4cfd2ea54ff3908209
 
-# Go to the directory from where the job was submitted (initial directory is $HOME)
-echo Working directory is $PBS_O_WORKDIR
-cd $PBS_O_WORKDIR
+# Go to the directory from where the job was submitted (initial directory is \$HOME)
+echo "Working directory is \$PBS_O_WORKDIR"
+cd \$PBS_O_WORKDIR
 
 ### Here follows the user commands:
 # Define number of processors
-NPROCS=`wc -l < $PBS_NODEFILE`
-echo This job has allocated $NPROCS nodes
+NPROCS=\`wc -l < \$PBS_NODEFILE\`
+echo This job has allocated \$NPROCS nodes
 
 # Load all required modules for the job
 module load tools perl edirect/7.50
 
 ### The real work begins here:
 # Go to the working_dr
-cd $HOME/ku_00039/people/dinghe/BLASTdb/EDirect
+cd \$HOME/ku_00039/people/dinghe/BLASTdb/EDirect
 
 # retrieve genomes
-for f in ${acc_dir}
+for f in \$acc_dir
 do
   echo "dowdloading genomes..."
-  epost -db nuccore -input $f -format acc -api_key $NCBI_API_KEY | efetch -format fasta > $f.fa
-  echo "$f"
+  epost -db nuccore -input \$f -format acc -api_key \$NCBI_API_KEY | efetch -format fasta > \$f.fa
+  echo \$f
 
   # try to avoid spamming the NCBI server
   sleep 2
 
   # retrieve proteomes
   echo "dowdloading proteomes..."
-  epost -db nuccore -input $f -format acc -api_key $NCBI_API_KEY | efetch -format fasta_cds_aa > $f.proteins.fa
-  echo "$f"
+  epost -db nuccore -input \$f -format acc -api_key \$NCBI_API_KEY | efetch -format fasta_cds_aa > \$f.proteins.fa
+  echo \$f
 done
 
 EOF
 
 # Submit the jobs
 qsub retrieve_PATRIC_prok_geneomes.qsub
-echo "Batch job $PBS_JOBID submitted"
+echo "Batch job \$PBS_JOBID submitted"
