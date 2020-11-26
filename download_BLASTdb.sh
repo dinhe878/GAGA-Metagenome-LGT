dea4cbd1a829634ec3c4e49829cda294cbca13ab
dinhe878 <derrick.he@gmail.com>
2020-11-26 22:11:30 +0100

    dev: now doing serial fasta retrieval instead of parallel

--- download_BLASTdb.sh
+++ download_BLASTdb.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# load the compoterome modules
-
 # go to the BLASTdb folder
 cd /home/people/dinghe/ku_00039/people/dinghe/BLASTdb/EDirect
+
+# make directories
+mkdir Prok_GenBankAcc
+acc_dir=./Prok_GenBankAcc
+mkdir Prok_Genome_fasta
+genome_dir=./Prok_Genome_fasta
+mkdir Prok_Proteome_fasta
+proteome_dir=./Prok_Proteome_fasta
 
 # Generate a list of complete bacterial genome accession numbers from PATRIC database selection. This results in 1908 genomes and a total of 3471 accession numbers (chromosome & plasmid)
 cat PATRIC_genome_list_21112020.txt | sed '1d' | awk -F '\t' '{print $0}' | cut -f 20 | sed 's/"//g' | sed 's/,/\n/g' > Prok_GenBankAcc.txt
 
-# Split the list into 13 smaller list, each has 267 accession numbers
-split -l 267 -d Prok_GenBankAcc.txt Prok_GenBankAcc.
+# Split the list into smaller lists of 10 accession numbers due to api_key access limit
+split -l 10 -d Prok_GenBankAcc.txt ${acc_dir}/Prok_GenBankAcc.
 
-# Generate qsub batch scripts for each list
-for n in {00..12}
-do
-
-cat > retrieve_PATRIC_prok_geneomes.${n}.qsub <<EOF
+# Generate qsub a batch script
+cat > retrieve_PATRIC_prok_geneomes.qsub <<EOF
 #!/bin/sh
 ### Note: No commands may be executed until after the #PBS lines
 ### Account information
 #PBS -W group_list=ku_00039 -A ku_00039
 ### Job name
-#PBS -N PATRIC_prok_geneome.${n}
+#PBS -N PATRIC_prok_geneome
 ### Output files
-#PBS -e PATRIC_prok_geneome.${n}.err
-#PBS -o PATRIC_prok_geneome.${n}.log
+#PBS -e PATRIC_prok_geneome.err
+#PBS -o PATRIC_prok_geneome.log
 ### Only send mail when job is aborted or terminates abnormally
 #PBS -m n
 ### Number of nodes/cores
@@ -32,7 +35,7 @@ cat > retrieve_PATRIC_prok_geneomes.${n}.qsub <<EOF
 ### Memory
 #PBS -l mem=10gb
 ### Requesting time - format is <days>:<hours>:<minutes>:<seconds>
-#PBS -l walltime=1:12:00:00
+#PBS -l walltime=10:00:00
 ### Set up the environmental variables
 #PBS -V $NCBI_API_KEY=593466ec18bd4cdf7c4cfd2ea54ff3908209
 
@@ -53,20 +56,23 @@ module load tools perl edirect/7.50
 cd $HOME/ku_00039/people/dinghe/BLASTdb/EDirect
 
 # retrieve genomes
-echo "dowdloading genomes..."
-epost -db nuccore -input Prok_GenBankAcc.${n} -format acc | efetch -format fasta > PATRIC_1908_prok_geneomes.${n}.fa
-echo "Genome dowdload completed"
+for f in ${acc_dir}
+do
+  echo "dowdloading genomes..."
+  epost -db nuccore -input $f -format acc -api_key $NCBI_API_KEY | efetch -format fasta > $f.fa
+  echo "$f"
 
-# retrieve proteomes
-echo "dowdloading proteomes..."
-epost -db nuccore -input Prok_GenBankAcc.${n} -format acc | efetch -format fasta_cds_aa > PATRIC_1908_prok_geneomes_proteins.${n}.fa
+  # try to avoid spamming the NCBI server
+  sleep 2
 
-echo "Proteome dowdload completed"
+  # retrieve proteomes
+  echo "dowdloading proteomes..."
+  epost -db nuccore -input $f -format acc -api_key $NCBI_API_KEY | efetch -format fasta_cds_aa > $f.proteins.fa
+  echo "$f"
+done
 
 EOF
 
 # Submit the jobs
-echo "Submitting the job $PBS_JOBID for the list PATRIC_prok_geneome.${n}..."
 qsub retrieve_PATRIC_prok_geneomes.${n}.qsub
-
-done
+echo "Batch job $PBS_JOBID submitted"
