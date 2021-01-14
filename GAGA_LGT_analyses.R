93bea1f633898c8de271107a49ffb2c1c9026a86
dinhe878 <derrick.he@gmail.com>
2021-01-14 23:17:02 +0100

    Add blastx processes

--- GAGA_LGT_analyses.R
+++ GAGA_LGT_analyses.R
@@ -15,15 +15,15 @@ library(DescTools)
 
 # GAGA ID
 args <- commandArgs(trailingOnly = T)
-if (length(args)==0) { args[1] <- "GAGA-0404" }
+if (length(args)==0) { args[1] <- "GAGA-0024" }
 id<-args[1]
 
 # local result folder
-folder<-paste("/Users/dinghe/Dropbox/Work/Projects/GAGA/Metagenome_analysis/LGT_pipeline/Results/",id,"/",sep="")
+folder<-paste("/Users/dinghe/Dropbox/Work/Projects/GAGA/Metagenome_analysis/LGT_pipeline/Results/Latest/14012021/",id,"/",sep="")
 
 # rRNA annotations 
-proRNA.tmp<-read.csv(paste(folder,"genome.pro.rRNA.windows.bed",sep=""),sep='\t',header=F,comment.char = "#")
-eukRNA.tmp<-read.csv(paste(folder,"genome.euk.rRNA.windows.bed",sep=""),sep='\t',header=F,comment.char = "#")
+proRNA.tmp<-read.csv(paste(folder,"genome.pro_blastn.rRNA.windows.bed",sep=""),sep='\t',header=F,comment.char = "#")
+eukRNA.tmp<-read.csv(paste(folder,"genome.euk_blastn.rRNA.windows.bed",sep=""),sep='\t',header=F,comment.char = "#")
 eukRNA.tmp<-eukRNA.tmp[,c(1:3,6,7,8,9,10,12)]
 proRNA.tmp<-proRNA.tmp[,c(1:3,6,7,8,9,10,12)]
 colnames(eukRNA.tmp)<-c("scf","start","end","eukRNA.type","eukRNA.start","eukRNA.end","eukRNA.eval","eukRNA.strand","eukRNA.att")
@@ -40,8 +40,12 @@ proRNA<-proRNA.tmp %>%
   dplyr::slice(1)
 
 # mmseqs blastn hits files
-euk<-read.csv(paste(folder,"query.", id, ".DB.euk.bh",sep=""),sep='\t',header=F)
-pro<-read.csv(paste(folder,"query.", id, ".DB.pro.bh",sep=""),sep='\t',header=F)
+euk<-read.csv(paste(folder,"query.", id, ".DB.euk_blastn.bh",sep=""),sep='\t',header=F)
+pro<-read.csv(paste(folder,"query.", id, ".DB.pro_blastn.bh",sep=""),sep='\t',header=F)
+
+# mmseqs blastx hits files
+euk.x<-read.csv(paste(folder,"query.", id, ".DB.euk_blastx.bh",sep=""),sep='\t',header=F)
+pro.x<-read.csv(paste(folder,"query.", id, ".DB.pro_blastx.bh",sep=""),sep='\t',header=F)
 
 #  GC content & window Count per scaffold
 GC<-read.csv(paste(folder,"genome.GC.tsv",sep=""),sep='\t',header=T)
@@ -56,8 +60,10 @@ cov$qseqid<-paste(cov$V1,":",cov$V2,"-",cov$V3,sep="")
 colnames(cov)[4]<-"cov"
 
 ## LGT Analysis
-# blastn vs prok database (2422 complete prokaryotic genomes)
-# blastn vs insect database (high quality complete genomes)
+# blastn vs prok database (NCBI: 1908 complete prokaryotic genomes)
+# blastn vs insect database (NCBI: 43 high quality complete genomes)
+# blastx vs prok database (Uniprot90 database - Bacteria part)
+# blastx vs insect database (Uniprot90 database - Insecta part)
 # rRNA annotations
 # genomic coverage from long-read data
 # GC content 
@@ -66,7 +72,9 @@ colnames(cov)[4]<-"cov"
 # name columns
 blsNcolnames<-c("qScf","qScfstart","qScfend","qseqid","qstart","qend","sseqid","sstart","send","evalue","bitscore","length","pident","sgi","sacc","stitle")
 colnames(euk)<-paste("euk.",blsNcolnames,sep="")
+colnames(euk.x)<-paste("euk.x.",blsNcolnames,sep="")
 colnames(pro)<-paste("pro.",blsNcolnames,sep="")
+colnames(pro.x)<-paste("pro.x.",blsNcolnames,sep="")
 colnames(GC)<-c("scaffold","Length","GC")
 
 # clean up prokaryotic taxonomy information
@@ -76,14 +84,38 @@ pro$pro.tax <- with(pro, ifelse(grepl(".*g_(.*?);.*", pro.sgi), gsub(".*g_(.*?);
                          ifelse(grepl(".*o_(.*?);.*", pro.sgi), gsub(".*o_(.*?);.*","\\1",pro.sgi),
                          ifelse(grepl(".*c_(.*?);.*", pro.sgi), gsub(".*c_(.*?);.*","\\1",pro.sgi),
                          ifelse(grepl(".*p_(.*?);.*", pro.sgi), gsub(".*p_(.*?);.*","\\1",pro.sgi),
-                                                                gsub(".*d_(.*?);.*","\\1",pro$pro.sgi)))))))
-euk$euk.tax<-gsub(".*g_(.*?);.*","\\1",euk$euk.sgi)
-euk$euk.tax[euk$euk.tax=="unclassified"]<-"Insecta"
+                                                                gsub(".*d_(.*?);.*","\\1",pro.sgi)))))))
+pro.x$pro.x.stitle<-gsub("Candidatus ","",pro.x$pro.x.stitle)
+pro.x$pro.x.tax <- with(pro.x, ifelse(grepl(".*g_(.*?);.*", pro.x.sgi), gsub(".*g_(.*?);.*","\\1",pro.x.sgi), 
+                             ifelse(grepl(".*f_(.*?);.*", pro.x.sgi), gsub(".*f_(.*?);.*","\\1",pro.x.sgi),
+                             ifelse(grepl(".*o_(.*?);.*", pro.x.sgi), gsub(".*o_(.*?);.*","\\1",pro.x.sgi),
+                             ifelse(grepl(".*c_(.*?);.*", pro.x.sgi), gsub(".*c_(.*?);.*","\\1",pro.x.sgi),
+                             ifelse(grepl(".*p_(.*?);.*", pro.x.sgi), gsub(".*p_(.*?);.*","\\1",pro.x.sgi),
+                                                                    gsub(".*d_(.*?);.*","\\1",pro.x.sgi)))))))
 
-# merge pro and euk blastn results
-m1<- merge(euk,pro,by.x="euk.qseqid",by.y="pro.qseqid", all.x = T,all.y=T) %>%
-  merge(.,eukRNA,by.x="euk.qseqid",by.y="qseqid", all.x = T,all.y=T) %>%
-  merge(.,proRNA,by.x="euk.qseqid",by.y="qseqid", all.x = T,all.y=T)
+euk$euk.tax <- with(euk, ifelse(grepl(".*g_(.*?);.*", euk.sgi), gsub(".*g_(.*?);.*","\\1",euk.sgi), 
+                         ifelse(grepl(".*f_(.*?);.*", euk.sgi), gsub(".*f_(.*?);.*","\\1",euk.sgi),
+                         ifelse(grepl(".*o_(.*?);.*", euk.sgi), gsub(".*o_(.*?);.*","\\1",euk.sgi),
+                         ifelse(grepl(".*c_(.*?);.*", euk.sgi), gsub(".*c_(.*?);.*","\\1",euk.sgi),
+                         ifelse(grepl(".*p_(.*?);.*", euk.sgi), gsub(".*p_(.*?);.*","\\1",euk.sgi),
+                                                                gsub(".*d_(.*?);.*","\\1",euk.sgi)))))))
+
+#euk$euk.tax[euk$euk.tax=="unclassified"]<-"Insecta"
+
+euk.x$euk.x.tax <- with(euk.x, ifelse(grepl(".*g_(.*?);.*", euk.x.sgi), gsub(".*g_(.*?);.*","\\1",euk.x.sgi), 
+                             ifelse(grepl(".*f_(.*?);.*", euk.x.sgi), gsub(".*f_(.*?);.*","\\1",euk.x.sgi),
+                             ifelse(grepl(".*o_(.*?);.*", euk.x.sgi), gsub(".*o_(.*?);.*","\\1",euk.x.sgi),
+                             ifelse(grepl(".*c_(.*?);.*", euk.x.sgi), gsub(".*c_(.*?);.*","\\1",euk.x.sgi),
+                             ifelse(grepl(".*p_(.*?);.*", euk.x.sgi), gsub(".*p_(.*?);.*","\\1",euk.x.sgi),
+                                                                    gsub(".*d_(.*?);.*","\\1",euk.x.sgi)))))))
+#euk.x$euk.x.tax[euk.x$euk.x.tax=="unclassified"]<-"Insecta"
+
+# merge pro and euk blastn/blastx results
+m1<- merge(euk,pro,by.x="euk.qseqid",by.y="pro.qseqid",all.x=T,all.y=T) %>%
+  merge(.,euk.x,by.x="euk.qseqid",by.y="euk.x.qseqid",all.x=T,all.y=T) %>%
+  merge(.,pro.x,by.x="euk.qseqid",by.y="pro.x.qseqid",all.x=T,all.y=T) %>%
+  merge(.,eukRNA,by.x="euk.qseqid",by.y="qseqid",all.x=T,all.y=T) %>%
+  merge(.,proRNA,by.x="euk.qseqid",by.y="qseqid",all.x=T,all.y=T)
 
 colnames(m1)[colnames(m1)=="euk.qseqid"]<-"qseqid"
 m1$scaffold<-gsub(":.*","",m1$qseqid,perl=T)
@@ -91,10 +123,14 @@ m1$start<-as.numeric(gsub(".*:(.*)-.*","\\1",m1$qseqid,perl=T))
 m1$end<-as.numeric(gsub(".*:.*-(.*)","\\1",m1$qseqid,perl=T))
 
 #select relevant columns to keep
-m2<-m1[,c("scaffold","start","end", "qseqid","euk.evalue","euk.bitscore","euk.pident","euk.sstart","euk.send","pro.evalue","pro.bitscore","pro.pident","euk.stitle","pro.stitle","pro.sstart","pro.send","pro.tax","euk.tax","eukRNA.eval","proRNA.eval","euk.qstart","euk.qend","pro.qstart","pro.qend","pro.sacc")]
+m2<-m1[,c("scaffold","start","end", "qseqid","euk.evalue","euk.bitscore","euk.pident",
+          "euk.sstart","euk.send","pro.evalue","pro.bitscore","pro.pident","euk.stitle",
+          "pro.stitle","pro.sstart","pro.send","pro.tax","euk.tax","eukRNA.eval",
+          "euk.x.bitscore","pro.x.bitscore","euk.x.tax","pro.x.tax",
+          "proRNA.eval","euk.qstart","euk.qend","pro.qstart","pro.qend","pro.sacc")]
 m2$scaffold<-as.factor(m2$scaffold)
 
-# merge blastn results with GC content, coverage and windows-wise GC content
+# merge blastn/x results with GC content, coverage and windows-wise GC content
 m4<-merge(m2,GC,by.x="scaffold",by.y="scaffold",all.x=T,all.y=T) %>% 
   merge(.,cov[,c("qseqid","cov")],by="qseqid",all.x=T) %>%
   merge(.,wGC[,c("qseqid","wX.GC")],by="qseqid",all.x=T)
@@ -107,21 +143,33 @@ m4$pro.tax[m4$eukRNA.eval>m4$proRNA.eval | (!is.na(m4$proRNA.eval) & is.na(m4$eu
 m4$pro.tax[m4$eukRNA.eval<m4$proRNA.eval | (is.na(m4$proRNA.eval) & !is.na(m4$eukRNA.eval))]<-"euk.rRNA"
 
 # Compute alignment lenghts
-m4$eukAln<-abs(m4$euk.send-m4$euk.sstart)
-m4$proAln<-abs(m4$pro.send-m4$pro.sstart)
+# Currently not in use
+#m4$eukAln<-abs(m4$euk.send-m4$euk.sstart)
+#m4$proAln<-abs(m4$pro.send-m4$pro.sstart)
 
-# Filter all windows with better hit against euk
+# (blastn) Filter all windows with better hit against euk
 eukWindows<-subset(m4,
                    (euk.bitscore>pro.bitscore  |   # is euk.bitscore higher than pro.bitscore
                       eukRNA.eval<proRNA.eval )  |   # is rRNA evalue higher agains pro than in euk
                      (!is.na(euk.bitscore) & is.na(pro.bitscore)) | # do we have no prok.bitscore but a euk.bitscore?
                      (!is.na(eukRNA.eval) & is.na(proRNA.eval)))    # do we have no prok.rRNA hit but a euk.rRNA hit?
-# Filter all windows with better hit against pro
+
+# (blastx) Filter all windows with better hit against euk
+eukWindows.x<-subset(m4,
+                      euk.x.bitscore>pro.x.bitscore |   # is euk.x.bitscore higher than pro.x.bitscore
+                      (!is.na(euk.x.bitscore) & is.na(pro.x.bitscore))) # do we have no prok.x.bitscore but a euk.x.bitscore?
+
+# (blastn) Filter all windows with better hit against pro
 proWindows<-subset(m4,   
                    (euk.bitscore<=pro.bitscore  |   # is euk.bitscore lower than pro.bitscore
                       eukRNA.eval>=proRNA.eval )  |   # is rRNA evalue lower agains pro than in euk
                      (is.na(euk.bitscore) & !is.na(pro.bitscore)) | # do we have a prok.bitscore but no euk.bitscore?
                      (is.na(eukRNA.eval) & !is.na(proRNA.eval)))    # do we have a prok.rRNA hit but no euk.rRNA hit?
+
+# (blastx) Filter all windows with better hit against pro
+proWindows.x<-subset(m4,
+                     euk.x.bitscore<=pro.x.bitscore |   # is euk.x.bitscore lower than pro.x.bitscore
+                       (is.na(euk.x.bitscore) & !is.na(pro.x.bitscore))) # do we have a prok.bitscore but no euk.bitscore?
 
 ### Identify rate of "prokaryotic windows" across entire chromosome
 # count windows by scaffold
@@ -136,14 +184,26 @@ eukWindowCount<-eukWindows %>%
   dplyr::summarise(no_rows = length(scaffold),bs=sum(euk.bitscore,na.rm=T))
 colnames(eukWindowCount)<-c("scaffold","eukWindows","euk.bitscore")
 
+eukWindowCount.x<-eukWindows.x %>% 
+  dplyr::group_by(scaffold) %>%
+  dplyr::summarise(no_rows = length(scaffold),bs=sum(euk.x.bitscore,na.rm=T))
+colnames(eukWindowCount.x)<-c("scaffold","eukWindows.x","euk.x.bitscore")
+
 # get number of windows producing significant hits vs prokDB
 proWindowCount<-proWindows %>% 
   dplyr::group_by(scaffold) %>%
   dplyr::summarise(no_rows = length(scaffold),bs=sum(pro.bitscore,na.rm=T))
 colnames(proWindowCount)<-c("scaffold","proWindows","pro.bitscore")
 
+proWindowCount.x<-proWindows.x %>% 
+  dplyr::group_by(scaffold) %>%
+  dplyr::summarise(no_rows = length(scaffold),bs=sum(pro.x.bitscore,na.rm=T))
+colnames(proWindowCount.x)<-c("scaffold","proWindows.x","pro.x.bitscore")
+
 chrSummary<-merge(GC,proWindowCount,by.x="scaffold",by.y="scaffold",all.y=T,all.x=T) %>% 
-  merge(.,eukWindowCount,by.x="scaffold",by.y="scaffold",all.x=T,all.y=T)
+  merge(.,proWindowCount.x,by.x="scaffold",by.y="scaffold",all.y=T,all.x=T) %>%
+  merge(.,eukWindowCount,by.x="scaffold",by.y="scaffold",all.x=T,all.y=T) %>%
+  merge(.,eukWindowCount.x,by.x="scaffold",by.y="scaffold",all.x=T,all.y=T)
 # we likely do not need to merge "windows" df?
 #%>% merge(.,windows,by.x="scaffold",by.y="scaffold",all.x=T,all.y=T) 
 
@@ -152,6 +212,9 @@ chrSummary[is.na(chrSummary)]<-0
 # calculate ratio of windows that have a better hit against the prokaryotic DB than against the eukaryotic DB
 chrSummary$ratio<-chrSummary$proWindows/(chrSummary$proWindows+chrSummary$eukWindows)
 chrSummary$bsratio<-chrSummary$pro.bitscore-chrSummary$euk.bitscore
+chrSummary$ratio.x<-chrSummary$proWindows.x/(chrSummary$proWindows.x+chrSummary$eukWindows.x)
+chrSummary$bsratio.x<-chrSummary$pro.x.bitscore-chrSummary$euk.x.bitscore
+chrSummary[is.na(chrSummary)]<-0
 
 ### Retrieve most frequently hit pro_taxon for each scaffold
 proWindowsList<-split(proWindows,f = proWindows$scaffold,drop = T)
@@ -195,8 +258,9 @@ chrSum<-merge(chrSummary,euk.bestMatch[,1:2],by.x="scaffold",by.y=".id",all.x=T)
 #   1. 100 % of hitted windows are identified as prokaryotic OR
 #   2. more than 50% of hitted windows are identified as prokaryotic AND bsratio > 200
 #   3. more than 50% of hitted windows are identified as prokaryotic AND 0 < bsratio < 200 AND thier
-#     3.1 GC content does not lie within 95% confidence interval of euk scaffolds GC content distribution AND
-#     3.2 coverage does not lie within 95% confidence interval of euk scaffolds coverage distribution 
+#     3.1 bsratio.x > 50 AND
+#     3.2 GC content does not lie within 95% confidence interval of euk scaffolds GC content distribution AND
+#     3.3 coverage does not lie within 95% confidence interval of euk scaffolds coverage distribution 
 # note that it is possible to have ratio 1 for only small amount of proWindow but the rest are uncertain (no hit)
 # for downstream genome annotation, only the scaffolds tagged "pro" should be excluded
 # for downstram microbiome analyses, include all "pro" and "uncertain" scaffolds
@@ -205,11 +269,11 @@ chrSum.euk <- chrSum %>% filter(ratio == 0)
 GC_medianCI.euk <- MedianCI(chrSum.euk$GC, method = "boot", conf.level = 0.95)
 Coverage_medianCI.euk <- MedianCI(chrSum.euk$coverage, method = "boot", conf.level = 0.95)
 
-chrSum$kingdom<-ifelse(is.na(chrSum$ratio), "unknown", 
+chrSum$kingdom<-ifelse(is.na(chrSum$ratio) & is.na(chrSum$bsratio.x), "unknown", 
                 ifelse(chrSum$ratio==1, "pro",
-                ifelse(chrSum$ratio<=0.5,"euk",
-                ifelse(chrSum$bsratio > 200, "pro",
-                ifelse(chrSum$bsratio > 0 & chrSum$GC<GC_medianCI.euk[2] & chrSum$GC>GC_medianCI.euk[3] &
+                ifelse(chrSum$ratio<=0.5, "euk",
+                ifelse(chrSum$bsratio>200, "pro",
+                ifelse(chrSum$bsratio>0 & chrSum$bsratio.x>=50 & chrSum$GC<GC_medianCI.euk[2] & chrSum$GC>GC_medianCI.euk[3] &
                 chrSum$coverage<Coverage_medianCI.euk[2] & chrSum$coverage>Coverage_medianCI.euk[3],"pro","uncertain")))))
 
 chrSum$type<-ifelse(chrSum$kingdom=="unknown", "unknown", 
