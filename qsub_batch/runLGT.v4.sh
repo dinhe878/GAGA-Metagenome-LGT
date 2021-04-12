0b0e3c36e56d8b0cfa1ec81cd1b7cd4126126f8e
dinhe878 <derrick.he@gmail.com>
2021-04-12 10:32:32 +0200

    Rscript: human contaminant detection fix sh: minimaps read file condition adjust

--- qsub_batch/runLGT.v4.sh
+++ qsub_batch/runLGT.v4.sh
@@ -176,7 +176,7 @@ echo "Gathering sequencing coverage information..."
 if [[ ${id} =~ ^GAGA.*$ ]]
 then
   minimap2 -t 40 -ax map-pb genome.fa ${raw_reads_dr}/fq/${id}.fq.gz > mapping/${id}.longread.sam
-elif [[ ((${id} =~ ^NCBI.*$) || (${id} =~ ^OUT.*$)) && (${tech} == "pair") ]]
+elif [[ ${tech} == "pair" ]]
 then
   minimap2 -t 40 -ax sr genome.fa ${raw_reads_dr}/fq/${id}_1.fq.gz ${raw_reads_dr}/fq/${id}_2.fq.gz > mapping/${id}.longread.sam
 elif [[ ((${id} =~ ^NCBI.*$) || (${id} =~ ^OUT.*$)) && (${tech} == "pacbio") ]]
