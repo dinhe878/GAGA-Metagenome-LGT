6bd7dd70b7a6a6a38d6de272015257da7e9fc3e0
schraderl <lukas.schrader@rub.de>
2021-04-15 11:54:38 +0200

    prepare for CR2 runs

--- runLGTfinder.noAnts.sh
+++ runLGTfinder.noAnts.sh
@@ -9,6 +9,17 @@ readlink -f /home/projects/ku_00039/people/dinghe/working_dr/metagenome_lgt/GAGA
 RMDpath=/home/projects/ku_00039/people/luksch/software/GAGA-Metagenome-LGT/analyseLGTs.cr2.Rmd
 outfolder=/home/projects/ku_00039/people/luksch/GAGA/LGT/
 base=/home/projects/ku_00039/people/dinghe/working_dr/metagenome_lgt/GAGA/
+id=GAGA-0001
+
+module purge
+module load tools gcc intel/perflibs R/4.0.3 pandoc/1.15
 
   Rscript -e "rmarkdown::render('${RMDpath}',output_file='${outfolder}/${id}.LGTfinder.html',params=list(id = '${id}',dir='${base}',type='euk'))"
   Rscript -e "rmarkdown::render('${RMDpath}',output_file='${outfolder}/${id}.LGTfinder.html',params=list(id = '${id}',dir='${base}',type='noAnt'))"
+
+
+#ggbio
+#bamsignals
+#randomcoloR
+#ggmsa
+# remove doParallel
